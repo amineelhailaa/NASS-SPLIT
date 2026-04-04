95f27d0e69bd741b10084dfd8682999132e6f793
AMINE <amineelhailaa@gmail.com>
2026-04-04 19:06:08 +0100

    feat(payment): add PaymentRequest for payment validation

---
+++ app/Http/Requests/Api/V1/PaymentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'creditor_id'=> 'exists:memberships',
+            'debtor_id'=> 'exists:memberships',
+            'amount'=> 'decimal:2'
+        ];
+    }
+}
