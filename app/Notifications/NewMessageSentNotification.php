4412d69654544ba25269ff65b121e23b6aaa0c41
AMINE <amineelhailaa@gmail.com>
2026-04-04 17:35:31 +0100

    feat(notification): create NewMessageSentNotification for message broadcasting

---
+++ app/Notifications/NewMessageSentNotification.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewMessageSentNotification extends Notification
+{
+    use Queueable;
+
+    public Message $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct( Message $message)
+    {
+        $this->message = $message;
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database','broadcast'];
+    }
+
+
+    //for database
+    public function toDatabase(Object $notifiable): array{
+        return [
+            'message_id'=> $this->message->id,
+            'conversation_id'=> $this->message->conversation_id,
+            'sender_id'=> $this->message->user_id,
+            'sender_name' => $this->message->user->name ,
+            'group_name'=>$this->message->conversation->group->name,
+        ];
+    }
+
+
+    //websocket
+        public function toBroadcast(object $notifiable): BroadcastMessage
+        {
+            return new BroadcastMessage([
+                'message_id'=> $this->message->id,
+                'conversation_id'=> $this->message->conversation_id,
+                'sender_id'=> $this->message->user_id,
+                'sender_name' => $this->message->user->name ,
+                'group_name'=>$this->message->conversation->group->name,
+            ]);
+        }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
