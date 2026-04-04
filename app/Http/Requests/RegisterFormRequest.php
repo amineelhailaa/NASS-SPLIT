02f94329cf04a90ffca1fad59c6cd49f32990ae1
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:28 +0100

    fix(request): fixing the RegisterFormRequest format and improve visibility with unified rule style

--- app/Http/Requests/RegisterFormRequest.php
+++ app/Http/Requests/RegisterFormRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterFormRequest extends FormRequest
@@ -17,11 +18,11 @@ class RegisterFormRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
-        return [ 'fullName' => 'required|min:3|max:50',
+        return ['fullName' => 'required|min:3|max:50',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8|max:50|confirmed',
             'password_confirmation' => 'required',
