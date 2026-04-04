c09050eec3d6aa0d5896254c841fa655c5937f9b
AMINE <amineelhailaa@gmail.com>
2026-04-04 19:06:02 +0100

    feat(payment): create PaymentController with store method for payment processing

---
+++ app/Http/Controllers/Api/V1/PaymentController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\ApiResponses;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\PaymentRequest;
+use App\Models\Payment;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    use ApiResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PaymentRequest $request)
+    {
+           $user = $request->user();
+           //need validation here dont forget it .
+        $user->memberships()->whereIn('id',
+            [$request->creditor_id,$request->debtor_id])->firstOrFail();
+
+         $payment = Payment::create([
+               'creditor_id' => $request->creditor_id,
+               'debtor_id' => $request->debtor_id,
+               'amount' => $request->amount,
+           ]);
+         return $this->createdResponse($payment);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
