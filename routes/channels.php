9dde08e144acdd350b7bacc1ccf5240c42998223
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:28 +0100

    fix(route): fixing the channels route format and improve visibility with structured channel definitions

--- routes/channels.php
+++ routes/channels.php
@@ -7,6 +7,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('conversation.{conversationId}',function (User $user, $conversationId){
+Broadcast::channel('conversation.{conversationId}', function (User $user, $conversationId) {
     return $user->conversations()->whereKey($conversationId)->exists();
 });
