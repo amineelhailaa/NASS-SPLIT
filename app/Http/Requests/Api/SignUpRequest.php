9aaeb085fbc9cc7e10eaafd653dd73fbc31c2abb
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:28 +0100

    fix(request): fixing the SignUpRequest format and improve visibility with neater rule definitions

--- app/Http/Requests/Api/SignUpRequest.php
+++ app/Http/Requests/Api/SignUpRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SignUpRequest extends FormRequest
@@ -17,14 +18,14 @@ class SignUpRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'name'=>'required|string|max:20',
-            'email'=>'required|email|unique:users,email',
-            'password'=>'required|string|min:8|confirmed'
+            'name' => 'required|string|max:20',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
         ];
     }
 }
