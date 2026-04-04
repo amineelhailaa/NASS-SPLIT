3cf8e65fdb6abbbbbba5175fa2ee91a4ab1ea55c
AMINE <amineelhailaa@gmail.com>
2026-04-04 19:05:31 +0100

    feat(group): add users relationship to Group model for active memberships

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
@@ -69,6 +70,15 @@ class Group extends Model
     public function members(): HasMany
     {
         return $this->hasMany(Membership::class, 'group_id');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class,
+        'memberships',
+        'group_id','user_id')
+            ->withPivot(['id','role','left_at','status'])
+            ->withTimestamps()->wherePivot('status','active');
     }
 
 
