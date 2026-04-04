bebf40b2e614809c1a02fd74b15c86e176ae4456
AMINE <amineelhailaa@gmail.com>
2026-04-04 17:03:09 +0100

    feat(broadcasting): add configuration files for broadcasting events and channels

---
+++ routes/channels.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('conversation.{conversationId}',function (User $user, $conversationId){
+    return $user->conversations()->whereKey($conversationId)->exists();
+});
