30c3d1a45392f361061ff3d2918c1022e93070ba
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:28 +0100

    fix(controller): fixing the GroupController format and improve visibility with unified style

--- app/Http/Controllers/Api/V1/GroupController.php
+++ app/Http/Controllers/Api/V1/GroupController.php
@@ -14,16 +14,15 @@ use Illuminate\Support\Facades\Gate;
 class GroupController extends Controller
 {
     use ApiResponses;
+
     /**
      * Display a listing of the resource.
      */
-
     public function __construct(
-       private GroupService $groupService,
+        private GroupService $groupService,
         private SettlementService $settlementService
-    ){
+    ) {}
 
-    }
     public function index(Request $request)
     {
         $is_admin = $request->user()->admin()->exists();
@@ -34,10 +33,11 @@ class GroupController extends Controller
             $searchTerm = $request->input('search');
             $query->where('name', 'like', "%$searchTerm%");
         }
-        //sort
+        // sort
         $sortBy = in_array($request->input('sort_by'), ['created_at', 'name']) ? $request->input('sort_by') : 'created_at';
         $sortType = $request->input('sort_dir') === 'asc' ? 'asc' : 'desc';
         $groups = $query->orderBy('groups.'.$sortBy, $sortType)->paginate(9);
+
         return $this->successResponse($groups);
     }
 
@@ -47,15 +47,15 @@ class GroupController extends Controller
     public function store(GroupFormRequest $request)
     {
 
-       $user = $request->user();
-       $group =  $this->groupService->createGroup($user,$request);
+        $user = $request->user();
+        $group = $this->groupService->createGroup($user, $request);
         if ($request->hasFile('avatar')) {
             $file = $request->file('avatar');
             $path = $file->store('avatars', 'public');
             $group->avatar()->create([
-                'path'=>$path,
+                'path' => $path,
                 'file_name' => $file->getClientOriginalName(),
-                'file_type' => $file->isImage() ? 'image':'file'
+                'file_type' => $file->isImage() ? 'image' : 'file',
             ]);
         }
 
@@ -68,7 +68,8 @@ class GroupController extends Controller
     public function show(string $id)
     {
         $group = Group::findOrFail($id);
-        Gate::authorize('member',$group);
+        Gate::authorize('member', $group);
+
         return $this->successResponse($group);
 
     }
@@ -78,23 +79,23 @@ class GroupController extends Controller
      */
     public function update(GroupFormRequest $request, string $id)
     {
-        $group =   Group::findOrFail($id);
-        Gate::authorize('owner',[$group]);
+        $group = Group::findOrFail($id);
+        Gate::authorize('owner', [$group]);
         $group->update([
             'name' => $request->name,
-            'description' => $request->description
+            'description' => $request->description,
         ]);
         if ($request->hasFile('avatar')) {
             $file = $request->file('avatar');
             $path = $file->store('avatars', 'public');
             $group->avatar()->update([
-                'path'=>$path,
+                'path' => $path,
                 'file_name' => $file->getClientOriginalName(),
-                'file_type' => $file->isImage() ? 'image':'file'
+                'file_type' => $file->isImage() ? 'image' : 'file',
             ]);
         }
 
-        return $this->successResponse($group,'group updated successuflly !');
+        return $this->successResponse($group, 'group updated successuflly !');
     }
 
     /**
@@ -103,53 +104,53 @@ class GroupController extends Controller
     public function destroy(string $id)
     {
         $group = Group::findOrFail($id);
-        Gate::authorize('owner',[$group]);
+        Gate::authorize('owner', [$group]);
         $group->delete();
-       return $this->successResponse([],'group deleted');
+
+        return $this->successResponse([], 'group deleted');
     }
 
+    public function members(Group $group)
+    {
+        Gate::authorize('member', $group);
 
-    public function members(Group $group){
-        Gate::authorize('member',$group);
-      return $this->successResponse($group->users()->with('avatar')->get());
+        return $this->successResponse($group->users()->with('avatar')->get());
     }
 
+    public function statistics(Group $group) {}
 
-    public function statistics(Group $group){
-
-    }
-
-    public function myBalance(Request $request,Group $group){
-        Gate::authorize('member',$group);
+    public function myBalance(Request $request, Group $group)
+    {
+        Gate::authorize('member', $group);
         $user = $request->user();
-        $membership = $user->memberships()->where('group_id',$group->id)->firstOrFail();
-       return $this->successResponse(
-           $membership->splitsAsCreditor()
-               ->where('splits.status','unpaid')
-               ->sum('amount')
-           - $membership->splitsAsDebtor()
-               ->where('splits.status','unpaid')
-               ->sum('amount'));
+        $membership = $user->memberships()->where('group_id', $group->id)->firstOrFail();
+
+        return $this->successResponse(
+            $membership->splitsAsCreditor()
+                ->where('splits.status', 'unpaid')
+                ->sum('amount')
+            - $membership->splitsAsDebtor()
+                ->where('splits.status', 'unpaid')
+                ->sum('amount'));
     }
 
-
-    public function owes(Request $request, Group $group){
+    public function owes(Request $request, Group $group)
+    {
         $user = $request->user();
-        Gate::authorize('member',$group);
-       $owes =  $this->settlementService->forGroup($group);
-       $membership = $user->memberships()->where('group_id',$group->id)->firstOrFail();
+        Gate::authorize('member', $group);
+        $owes = $this->settlementService->forGroup($group);
+        $membership = $user->memberships()->where('group_id', $group->id)->firstOrFail();
 
-       $owes = array_values(array_filter($owes,function ($owe) use ($membership) {
-          return in_array($membership->id,[$owe['creditor_id'],$owe['debtor_id']]) ;
-       }));
+        $owes = array_values(array_filter($owes, function ($owe) use ($membership) {
+            return in_array($membership->id, [$owe['creditor_id'], $owe['debtor_id']]);
+        }));
+        $members = $group->members()->with('user.avatar')->get()->keyBy('id'); // make an assoc with ids inside
+        $owes = array_map(fn ($owe) => [
+            'creditor' => $members[$owe['creditor_id']]->user,
+            'debtor' => $members[$owes['debtor_id']]->user,
+            'amount' => $owe['amount'],
+        ], $owes);
 
-       $members = $group->members()->with('user.avatar')->get()->keyBy('id'); // make an assoc with ids inside
-        $owe = array_map(fn($owe)=>[
-            'creditor'=> $members[$owe['creditor_id']]->user,
-            'debtor'=> $members[$owes['debtor_id']]->user,
-            'amount'=> $owe['amount'],
-        ],$owes);
-       return $this->successResponse($owes);
+        return $this->successResponse($owes);
     }
-
 }
