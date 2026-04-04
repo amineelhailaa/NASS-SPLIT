8a3073070ce11d907e3d884d1637d8c8828ec926
AMINE <amineelhailaa@gmail.com>
2026-04-04 04:23:31 +0100

    feat(gates): define gates for group ownership and membership validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Group;
+use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +24,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        //gates:
+        Gate::define('owner', function (User $user, Group $group) {
+            return $user->groups()->whereKey($group->id)->wherePivot('role', 'owner')->where('status','active')->exists();
+        });
+        Gate::define('member', function (User  $user, Group $group) {
+            return $user->groups()->whereKey($group->id)->wherePivot('status','active')->exists();
         });
     }
+
 }
