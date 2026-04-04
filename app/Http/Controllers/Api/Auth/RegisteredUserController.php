827c63f1787330d6ea1a50f0c0d64988d5441d6f
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:27 +0100

    fix(controller): fixing the RegisteredUserController format and improve visibility with clear separation

--- app/Http/Controllers/Api/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Api/Auth/RegisteredUserController.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\SignUpRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
 
 class RegisteredUserController extends Controller
@@ -29,17 +27,16 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->string('password')),
         ]);
 
-        if ($request->hasFile('avatar')){
+        if ($request->hasFile('avatar')) {
             $file = $request->file('avatar');
-            $path = $file->store('','public');
+            $path = $file->store('', 'public');
 
             $user->avatar()->create([
-                'path'=> $path,
-                'file_type'=> $file->isImage() ? 'image' : 'file',
-                'file_name'=> $file->getClientOriginalName()
+                'path' => $path,
+                'file_type' => $file->isImage() ? 'image' : 'file',
+                'file_name' => $file->getClientOriginalName(),
             ]);
         }
-
 
         event(new Registered($user));
 
