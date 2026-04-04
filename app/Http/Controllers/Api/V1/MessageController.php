6bbeb8f1e8db53c4b3682eeef7b5dc2e5f303bdb
AMINE <amineelhailaa@gmail.com>
2026-04-04 03:41:36 +0100

    feat(api): add MessageController for handling message storage and retrieval

---
+++ app/Http/Controllers/Api/V1/MessageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\ApiResponses;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\MessageRequest;
+use App\Models\Conversation;
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    use ApiResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(MessageRequest $request,Conversation $conversation)
+    {
+        $user = $request->user();
+        abort_unless(
+            $user->conversations()->whereKey($conversation->id)->exists(), 404
+        );
+
+        $message = $conversation->messages()->create([
+            'user_id' => $user->id,
+            'message' => $request->input('message'),
+        ]);
+
+        return $this->createdResponse($message);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Message $message, Request $request)
+    {
+        $user = $request->user();
+        $user->messages()->where('messages.id',$message->id)->delete();
+        return $this->noContentResponse();
+    }
+}
