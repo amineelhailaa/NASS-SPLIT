136c359000f460113736b477775932348835136b
AMINE <amineelhailaa@gmail.com>
2026-04-04 04:59:30 +0100

    refactor(request): move GroupFormRequest to Api\V1 namespace and update import in GroupController

--- app/Http/Controllers/Api/V1/GroupController.php
+++ app/Http/Controllers/Api/V1/GroupController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\ApiResponses;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\GroupFormRequest;
+use App\Http\Requests\Api\V1\GroupFormRequest;
 use App\Models\Group;
 use App\Services\GroupService;
 use Illuminate\Http\Request;
