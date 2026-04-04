70004b4eee25310e0c03844a5986b3fa9b85e2f9
AMINE <amineelhailaa@gmail.com>
2026-04-04 23:15:28 +0100

    fix(controller): fixing the ExpenseController format and improve visibility with structured layout

--- app/Http/Controllers/Api/V1/ExpenseController.php
+++ app/Http/Controllers/Api/V1/ExpenseController.php
@@ -6,7 +6,6 @@ use App\ApiResponses;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\ExpenseFormRequest;
 use App\Http\Resources\V1\ExpenseResource;
-use App\Models\Expense;
 use App\Models\Group;
 use App\Models\Split;
 use App\Services\StrategyManagerService;
@@ -16,35 +15,35 @@ use Illuminate\Support\Facades\Gate;
 class ExpenseController extends Controller
 {
     use ApiResponses;
+
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request, $id)
     {
         $group = Group::findOrFail($id);
-        Gate::authorize('member',$group);
-        return ExpenseResource::collection(
-            $this
-                ->successResponse($group
-                    ->expenses()
-                    ->with('payer,category,attachments')
-                    ->get())
-        );
+        Gate::authorize('member', $group);
+
+        return ExpenseResource::collection($this
+            ->successResponse($group
+                ->expenses()
+                ->with('payer,category,attachments')
+                ->get()));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(ExpenseFormRequest $request,Group $group,StrategyManagerService $service)
+    public function store(ExpenseFormRequest $request, Group $group, StrategyManagerService $service)
     {
-        Gate::authorize('member',$group);
+        Gate::authorize('member', $group);
         $user = $request->user();
-       $expense = $group->expenses()->create([
+        $expense = $group->expenses()->create([
             'payer_id' => $request->payer_id,
             'category_id' => $request->category_id,
             'title' => $request->title,
-            'date'=> $request->date,
-            'amount' => $request->amount
+            'date' => $request->date,
+            'amount' => $request->amount,
         ]);
         if ($request->hasFile('attachments')) {
             foreach ($request->file('attachments') as $file) {
@@ -52,26 +51,24 @@ class ExpenseController extends Controller
                 $path = $file->store('', 'public');
 
                 $expense->attachments()->create([
-                    'path'=> $path,
-                    'file_type'=> $file->isImage() ? 'image' : 'file',
-                    'file_name'=> $file->getClientOriginalName()
+                    'path' => $path,
+                    'file_type' => $file->isImage() ? 'image' : 'file',
+                    'file_name' => $file->getClientOriginalName(),
                 ]);
             }
         }
-        $splits = $service->dataToInsert($request->split_strategy,$request->amount,$request->participants);
-        //now im looking for a function to insert collection without looping
+        $splits = $service->dataToInsert($request->split_strategy, $request->amount, $request->participants);
+        // now im looking for a function to insert collection without looping
         $now = now();
-        $splits = array_map(fn($item)=>[
-            ...$item,'expense_id'=> $expense->id,
-            'created_at'=> $now,
-            'updated_at'=>$now
-        ],$splits);
+        $splits = array_map(fn ($item) => [
+            ...$item, 'expense_id' => $expense->id,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ], $splits);
         Split::insert($splits);
-        return $this->createdResponse($expense,'created successfully');
+
+        return $this->createdResponse($expense, 'created successfully');
     }
-
-
-
 
     /**
      * Display the specified resource.
