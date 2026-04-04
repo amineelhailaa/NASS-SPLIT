777debc29e08d1ef8740220f4200bd18f2053fe9
AMINE <amineelhailaa@gmail.com>
2026-04-04 03:41:14 +0100

    feat(api): add routes for storing and deleting messages in MessageController

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,1 +1,9 @@
 <?php
+
+
+use App\Http\Controllers\Api\V1\MessageController;
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/conversation/{conversation}', [MessageController::class, 'store']);
+    Route::delete('/conversation/{message}', [MessageController::class,'destroy']);
+});
