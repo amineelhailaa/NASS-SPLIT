dab49ccda9e064839e5648d7950135839dc7277d
AMINE <amineelhailaa@gmail.com>
2026-04-05 01:18:40 +0100

    feat(routes): expand API routes for profile, groups, expenses, payments, conversations, messages, and categories

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,13 +1,57 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\ConversationController;
+use App\Http\Controllers\Api\V1\ExpenseController;
+use App\Http\Controllers\Api\V1\GroupController;
 use App\Http\Controllers\Api\V1\MessageController;
+use App\Http\Controllers\Api\V1\PaymentController;
+use App\Http\Controllers\Api\V1\ProfileController;
 
 Route::middleware('auth:sanctum')->group(function () {
+    // profile:
+    Route::get('/profile', [ProfileController::class, 'edit']);
+    Route::patch('/profile', [ProfileController::class, 'update']);
+
+    // Group:
+    Route::get('/groups', [GroupController::class, 'index']);
+    Route::post('/groups', [GroupController::class, 'store']);
+    Route::get('/groups/{id}', [GroupController::class, 'show']);
+    Route::patch('/groups/{id}', [GroupController::class, 'update']);
+    Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+    Route::get('/groups/{group}/members', [GroupController::class, 'members']);
+    Route::get('/groups/{group}/balance', [GroupController::class, 'myBalance']);
+    Route::get('/groups/{group}/owes', [GroupController::class, 'owes']);
+
+    // Expense:
+    Route::get('/groups/{id}/expenses', [ExpenseController::class, 'index']);
+    Route::post('/groups/{group}/expenses', [ExpenseController::class, 'store']);
+    Route::get('/expenses/{id}', [ExpenseController::class, 'show']);
+    Route::delete('/expenses/{id}', [ExpenseController::class, 'destroy']);
+
+    // Pyament:
+    Route::get('/payments', [PaymentController::class, 'index']);
+    Route::post('/payments', [PaymentController::class, 'store']);
+    Route::get('/payments/{id}', [PaymentController::class, 'show']);
+
+
+    // conversation
+    Route::get('/conversations', [ConversationController::class, 'index']);
+    Route::get('/conversations/{conversation}', [ConversationController::class, 'show']);
+
+    // message
     Route::post('/conversations/{conversation}', [MessageController::class, 'store']);
     Route::delete('/messages/{message}', [MessageController::class, 'destroy']);
+
+    // categories
+    Route::get('/categories', [CategoryController::class, 'index']);
+
+    // admin
+    Route::middleware('can:admin')->group(function () {
+        // categories:
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+        Route::patch('/categories/{category}', [CategoryController::class, 'update']);
+
+    });
 });
-
-
-
-
-
